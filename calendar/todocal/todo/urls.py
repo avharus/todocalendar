28c4fde62e4c719495f4bba00933ffed5449c397
Wouter Leeuw <Wouter Leeuw>
2017-01-11 20:50:42 +0100

    Modified urls.py

---
+++ calendar/todocal/todo/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url, include
+# from django.views.generic import ListView, DetailView
+from todo import views
+from .models import Task, Category, User
+
+urlspatterns = [
+
+]
