87250ca666db3f0cb59e13cbe6094f3b08682822
Wouter Leeuw <Wouter Leeuw>
2017-01-12 14:49:18 +0100

    Modified settings.py, urls.py

--- calendar/todocal/todocal/urls.py
+++ calendar/todocal/todocal/urls.py
@@ -25,6 +25,7 @@ router.register(r'categories', views.CategoryViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
     # url(r'^$', include(todo.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
